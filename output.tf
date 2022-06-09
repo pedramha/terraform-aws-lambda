79287f9b263befdc7f9ca11a3c3c51614c92e8c3
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-06-09 19:09:46 +0200

    amend

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "lambda_arn" {
+  value       = aws_lambda_function.lambdaFunc.arn
+  description = "value of the lambda function arn"
+}
+
+output "lambda_name" {
+  value       = aws_lambda_function.lambdaFunc.function_name
+  description = "value of the lambda function name"
+}
+
+output "provisioned_concurrent_executions" {
+  value       = aws_lambda_provisioned_concurrency_config.config.provisioned_concurrent_executions
+  description = "value of the lambda function provisioned concurrent executions"
+}
+  
+output "concurrent_executions" {
+  value       = aws_lambda_function.lambdaFunc.reserved_concurrent_executions
+  description = "value of the lambda function concurrent executions"
+}
